8daa239db694ce71c669e91bff9b1e26339043ae
Sam Rose <sam@floxdev.com>
2022-06-29 11:19:27 -0400

    nginx

--- flake.nix
+++ flake.nix
@@ -1,31 +1,36 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
   inputs.hydra.url = "github:NixOS/hydra";
-  outputs = { self, nixpkgs, hydra }: {
-
+  outputs = {
+    self,
+    nixpkgs,
+    hydra,
+  }: {
     nixosConfigurations.container = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules =
-        [ hydra.nixosModules.hydraTest
-	    ({ pkgs, ... }: {
-            boot.isContainer = true;
+      modules = [
+        hydra.nixosModules.hydraTest
+        ({pkgs, ...}: {
+          boot.isContainer = true;
 
-            # Let 'nixos-version --json' know about the Git revision
-            # of this flake.
-            system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+          # Let 'nixos-version --json' know about the Git revision
+          # of this flake.
+          system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
 
-            # Network configuration.
-            networking.useDHCP = false;
-            networking.firewall.allowedTCPPorts = [ 80 3000 ];
+          # Network configuration.
+          networking.useDHCP = false;
+          networking.firewall.allowedTCPPorts = [80 3000];
 
-            # Enable a web server.
-            services.httpd = {
-              enable = true;
-              adminAddr = "samuel.rose@gmail.com";
+          # Enable a web server.
+          services.nginx = {
+            enable = true;
+            virtualHosts.default = {
+              serverName = "flake-test";
+              locations."/".proxyPass = "http://flake-test:3000/";
             };
-          })
-        ];
+          };
+        })
+      ];
     };
-
   };
 }
